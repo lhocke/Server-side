2a197775d7bf4bd24a22880cb60b252beb4a9831
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-07-19 11:47:24 -0700

    adding html for fs

---
+++ server-fs.js
@@ -0,0 +1,52 @@
+var html = require("html");
+var url = require("url");
+var fs = require("fs");
+
+var PORT = 7000;
+
+function handleRequest(req, res) {
+    var urlParts = url.parse(url.res);
+
+    switch (urlParts.pathname) {
+        case "/":
+        displayIndex(urlParts.pathname, req, res)
+        break;
+        case "/food":
+        displayFood(urlParts.pathname, req, res)
+        break;
+        case "/movies":
+        displayMovies(urlParts.pathname, req, res)
+        break;
+        case "/frameworks":
+        displayFrameworks(url.pathname, req, res)
+        break;
+    }
+}
+
+function displayIndex(url, req, res) {
+    fs.readFile(__dirname + "/index.html", function(err, data){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(data);
+    });
+
+}
+
+function displayMovie(url, req, res) {
+    fs.readFile(__dirname + "/movies.html", function(err, data){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(data);
+    });
+}
+ function displayFood(url, req, res) {
+    fs.readFile(__dirname + "/food.html", function(err, data){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(data);
+    });
+}
+
+function displayFrameworks(url, req, res) {
+    fs.readFile(__dirname + "/frameworks.html", function(err, data){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(data);
+    });
+}
