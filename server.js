8fc068ab739dec2f158825f6d32008aa7b928be6
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-07-19 10:48:23 -0700

    initial commit

--- server.js
+++ server.js
@@ -12,8 +12,51 @@
 //   * Generate the good/bad phrase randomly from a list of predefined phrases
 //   * Use the `twitter` package inside the response to also return a random tweet
 
+// // Here we require/import the HTTP module
+// var http = require("http");
+
+// // Here we define a port to listen to
+// var PORT = 8080;
+
+// // Here we create a generic function to handle requests and responses
+// function handleRequest(request, response) {
+
+//   // The below statement is triggered (client-side) when the user visits the PORT URL
+//   response.end("It Works!! Path Hit: " + request.url);
+// }
+
+// // Here we use the Node HTTP package to create our server.
+// // We then pass it the handleRequest function to empower it with functionality.
+// var server = http.createServer(handleRequest);
+
+// // Here we start our server so that it can begin listening to client requests.
+// server.listen(PORT, function() {
+
+//   // The below statement is triggered (server-side) when a user visits the PORT URL
+//   console.log("Server listening on: http://localhost:%s", PORT);
+
+// });
+
 var http = require('http');
 
 var firstPORT = 7000;
-var secondPort = 7500;
+var secondPORT = 7500;
 
+function handleGoodRequest(response, result) {
+    response.end("You look great!")
+};
+
+function handleBadRequest(response, result) {
+    response.end("You look awful!")
+}
+
+var goodServer = http.createServer(handleGoodRequest);
+var badServer = http.createServer(handleBadRequest);
+
+goodServer.listen(firstPORT, function() {
+    console.log("good server connected")
+})
+
+badServer.listen(secondPORT, function() {
+    console.log("bad server connected")
+})
